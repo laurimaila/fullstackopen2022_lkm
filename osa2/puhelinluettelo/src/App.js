4c0e89b521b37a4070f72583b200c93e706fb1ec
Lauri Maila <laurikmaila@gmail.com>
2023-01-16 21:32:22 +0200

    2.11 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [contacts, setContacts] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setContacts(response.data)
+      })
+  }, [])
+  console.log('render', contacts.length, 'contacts')
 
   const addNumber = (event) => {
     event.preventDefault()
